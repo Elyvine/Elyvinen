4240e8ef19789b4b0ebe1e0cf4f9e4d0ada0d2d6
Elyvine <89011215+Elyvine@users.noreply.github.com>
2021-08-16 13:11:48 -0700

    Initial commit

---
+++ bookstore.cpp
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>The Books Lover Read all About it!</title>
+</head>
+<body>
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#books">Ebooks</a>
+  <a href="#about">About</a>
+  <a href="#contact">Contact</a>
+  <a href="#account">Account</a>
+</div>
+<p>Lorem ipsum dolor sit amet consectetur, adipisiscing elit incident voluplat porro nisi beataeinvontore consequuntur?</p>
+<img src="header-pic" alt="">
+<img src="Book 1.jpg" >
+<img src="Book 2.jpg" >
+<img src="Book 3.jpg" >
+<h2> Featured Titles</h2>
+
+
+
+</body>
+</html>
